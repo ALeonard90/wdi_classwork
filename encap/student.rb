f1b762489f870b928c7d420238f3cb2ef61df7f2
mattcho <dongukcho@gmail.com>
2014-11-26 09:17:28 -0500

    regular commit

--- encap/student.rb
+++
@@ -1,6 +0,0 @@
-class Student < Person
-	def turn_in_hw
-		value = "a"*rand(140)
-		return value
-	end
-end
--- student.rb
+++
@@ -1,9 +0,0 @@
-class Student
-	def says_hello
-		return 'Oh hay!!'
-	end
-end
-
-pam = Student.new
-
-pam.says_hello
